1556546d6d4d8d2627443572848939d3235641b4
asus_ocis <84299285+asus-ocis@users.noreply.github.com>
2022-06-24 17:47:45 +0800

    Create nvflare21.sh for nvflare version 2.1.
    Create nvflare21.sh for nvflare version 2.1.

---
+++ nvflare21.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Run nvflare21 script ..."
+echo "install nvflare"
+pip install nvflare==2.1.2 Pillow==8.2.0
+
+if [ "$(pip list | awk '/pytorch/ {print }'|wc -l)" -ge 1 ]; then
+  pip install monai
+else
+  echo "pytorch not found"
+fi
+
+pip install --upgrade "protobuf<=3.20.1"
+chmod +x $DIR/*.sh
+$DIR/bootstrap.sh
